727a8cb38fc85358c24492cb3cb17f02aa020ad6
CalvinNFT <101669393+CalvinNFT@users.noreply.github.com>
2024-11-09 04:24:07 +0200

    Bugfix: Init script

---
+++ be/database/config/config.js
@@ -0,0 +1,4 @@
+// database/config/config.js
+require('ts-node/register');
+const config = require('./config.ts').default;
+module.exports = config;
--- be/database/tools/terminateConnections.js
+++ be/database/tools/terminateConnections.js
@@ -2,8 +2,8 @@ const Sequelize = require('sequelize');
 const config = require('db/config/config');
 
 const terminateConnections = async () => {
-  const username = config.username;
-  const password = config.password;
+  const { username, password, database } = config; // Adjust to match your environment structure
+
   let sequelize;
   try {
     sequelize = new Sequelize(config.database, username, password, config);
@@ -34,13 +34,15 @@ const terminateConnections = async () => {
       await sequelize.query(`
         SELECT pg_terminate_backend(pid)
         FROM pg_stat_activity
-        WHERE datname = 'db' AND pid <> pg_backend_pid();
+        WHERE datname = '${database}' AND pid <> pg_backend_pid();
       `);
 
-      console.log(`Active connections to db terminated.`);
+      console.log(`Active connections to the database '${database}' have been terminated.`);
+    } else {
+      console.log(`No active connections found for the database '${database}'.`);
     }
-  } catch {
-    console.log('Error terminating connections');
+  } catch (error) {
+    console.error(error);
   } finally {
     await sequelize.close();
   }
