5835af947e7e0d444021dc0605050e51df29ef09
CalvinNFT <101669393+CalvinNFT@users.noreply.github.com>
2024-11-09 23:28:15 +0200

    Bugfix: Page imported that does not exit

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,7 +1,6 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import CreateBuilding from "./pages/CreateBuilding";
 import BuildingsList from "./pages/BuildingList";
-import ViewItems from "./pages/ViewItems";
 
 function App() {
   return (
@@ -9,8 +8,6 @@ function App() {
       <Routes>
         <Route path="/properties" element={<BuildingsList />} />
         <Route path="/properties/create" element={<CreateBuilding />} />
-        {/* <Route path="/properties/edit/:id" element={<EditBuilding />} /> */}
-        <Route path="/properties/:id/items" element={<ViewItems />} />
         <Route path="*" element={<Navigate to="/properties" />} />
       </Routes>
     </>
