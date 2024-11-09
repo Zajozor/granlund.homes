bff191c15dd1944b865a80633af570a5c399910f
CalvinNFT <101669393+CalvinNFT@users.noreply.github.com>
2024-11-09 23:48:26 +0200

    Feature: CO script to pushing branch in main

--- fe/scripts/push_to_main.sh
+++ fe/scripts/push_to_main.sh
@@ -1,41 +1,11 @@
 set -e
 
-branch=$(git rev-parse --abbrev-ref HEAD)
-message=$1
-
-if [ -z "$message" ]; then
-  echo "Please provide a commit message"
-  exit 1
-fi
-
-HEAD=$2
-
-if [ -z "$HEAD" ]; then
-  echo "Please provide number of HEAD commits to squash"
-  exit 1
-fi
-
-echo "Squashing $HEAD commits"
-git rebase HEAD~$HEAD
-
-echo 'making a commit'
-git add .
-git commit -m $message
-
-echo "Cheking out main"
 git checkout main
-
-echo "Pulling from main"
 git pull
-
-echo "Rebasing $branch onto main"
-git checkout $branch
-
-echo "Rebasing $branch to main"
+git checkout feature/desktop-client
 git rebase main
-
-echo "Pushing $branch to main"
-git checkout main && git merge $branch --squash && git commit -m $message && git push
-
-git checkout $branch
-echo "Pushed to main. Op finished."
+git push --force-with-lease
+git checkout main
+git merge feature/desktop-client --squash
+git push
+git checkout feature/desktop-client
