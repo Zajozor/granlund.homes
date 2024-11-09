f2b00b5531d4ad69c7d3f6ef939e0414f9df1596
CalvinNFT <101669393+CalvinNFT@users.noreply.github.com>
2024-11-09 05:21:19 +0200

    Last nightly changes

--- be/database/database.ts
+++ be/database/database.ts
@@ -1,6 +1,9 @@
 import { Sequelize } from 'sequelize-typescript';
 import dbConfig from './config/config';
 
-const sequelize = new Sequelize(dbConfig);
+const sequelize = new Sequelize({
+  ...dbConfig,
+  models: [__dirname + '/models'], // or [Player, Team],
+});
 
 export default sequelize;
