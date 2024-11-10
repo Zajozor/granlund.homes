ae2cc9c5ffa72471c1b70b1edf781e83a453d71f
CalvinNFT <101669393+CalvinNFT@users.noreply.github.com>
2024-11-10 09:49:18 +0200

    .

--- fe/src/components/Navbar.tsx
+++ fe/src/components/Navbar.tsx
@@ -4,15 +4,15 @@ import { Button, Text } from '@radix-ui/themes';
 import qr from '../assets/qr.svg';
 
 const ResponsiveNavbar = () => (
-  <div style={{ display: 'flex', justifyContent: 'space-between', height: 69, color: '#fff', alignItems: 'center', paddingRight: 42, paddingLeft: 42, borderRadius: 20 }}>
+  <div style={{ display: 'flex', justifyContent: 'space-between', height: 69, alignItems: 'center', paddingRight: 42, paddingLeft: 42, borderRadius: 20, opacity: 0.6}}>
     <a href='/'>
       <img src={logoSrc} alt='Granlund logo' width={100} height={40} />
     </a>
     <Popover.Root>
-      <Popover.Trigger>
-        <Button variant="soft">Add a member</Button>
+      <Popover.Trigger asChild>
+        <Button>Add a member</Button>
       </Popover.Trigger>
-      <Popover.Content maxWidth="400px">
+      <Popover.Content>
         <img 
           src={qr}
           alt="qr code" 
